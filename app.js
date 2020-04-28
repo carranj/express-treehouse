8ea29ace3ad56aeb6c079f0a87dd13a1995fac4d
Jose Carranco <carranco.jose.r@gmail.com>
2020-04-28 12:14:28 -0500

    The Request Object

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.set('view engine', 'pug');
 
@@ -8,17 +11,17 @@ app.get('/', (req , res) => {
     res.render('index');
 });
 
+app.get('/cards', (req , res) => {
+    res.render('card', {prompt: "Who is buried in Grant's tomb"});
+});
+
 app.get('/hello', (req , res) => {
     res.render('hello');
 });
 
 app.post('/hello', (req , res) => {
+    console.dir(req.body);
     res.render('hello');
-});
-
-app.get('/cards', (req , res) => {
-    res.locals.prompt = "Who is buried in Grant's tomb";
-    res.render('card', {prompt: "Who is buried in Grant's tomb"});
 });
 
 app.listen(3000, () =>{
