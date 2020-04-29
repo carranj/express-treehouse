b846e9102952a9de28de6b6e80994dcba513ab01
Jose Carranco <carranco.jose.r@gmail.com>
2020-04-29 18:19:04 -0500

    Handling 404 errors

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ app.set('view engine', 'pug');
 
 app.use( (req, res, next) => {
     console.log("Hello");
-    const err = new Error("Oh noes!");
-    err.status = 500;
-    next(err);
+    next();
 });
 
 app.use((req, res, next) => {
@@ -47,6 +45,12 @@ app.post('/hello', (req , res) => {
 app.post('/goodbye', (req , res) => {
     res.clearCookie('username')
     res.redirect('/hello');
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 app.use((err, req, res, next) => {
