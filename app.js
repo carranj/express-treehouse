edebe894877a4eef26e088c862d21d1594c1471e
Jose Carranco <carranco.jose.r@gmail.com>
2020-04-29 19:13:57 -0500

    Modular Routes

--- app.js
+++ app.js
@@ -9,43 +9,11 @@ app.use(cookierParser());
 
 app.set('view engine', 'pug');
 
-app.use( (req, res, next) => {
-    console.log("Hello");
-    next();
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
-app.use((req, res, next) => {
-    console.log("World!");
-    next();
-});
- 
-
-
-
-app.get('/', (req , res) => {
-    const name = req.cookies.username;
-    name? res.render('index', {name}): res.redirect('/hello');
-    
-});
-
-app.get('/cards', (req , res) => {
-    res.render('card', {prompt: "Who is buried in Grant's tomb"});
-});
-
-app.get('/hello', (req , res) => {
-    const name = req.cookies.username;
-    !name ? res.render('hello') : res.redirect('/');
-});
-
-app.post('/hello', (req , res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-app.post('/goodbye', (req , res) => {
-    res.clearCookie('username')
-    res.redirect('/hello');
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
